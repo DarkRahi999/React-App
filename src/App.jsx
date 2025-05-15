ae1ec30e2ba8959ed3f31f48458847bb55925d0b
Bisakto_Rahi <bisaktorahi007@gmail.com>
2025-05-15 11:58:32 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import {Body} from './components'
+import './CSS/style.css'
+function App() {
+  return (
+    <>
+    <Body></Body>
+    </>
+  )
+}
+export default App
---
+++ src/components/index.jsx
@@ -0,0 +1,8 @@
+export function Body(){
+    return(
+        <div>
+            <h2>Set up Ready</h2>
+            <h2>all ok</h2>
+        </div>
+    )
+}
